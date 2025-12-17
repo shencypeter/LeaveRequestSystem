2030911aaf4778af406254d45a1e5edc0a0ad823
Chen,ChunLiang <peter60146@gmail.com>
2025-12-17 14:35:44 +0800

    其他compiler內容

--- BioMedDocManager/obj/Debug/net8.0/BioMedDocManager.AssemblyInfo.cs
+++ BioMedDocManager/obj/Debug/net8.0/BioMedDocManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioMedDocManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f2a33bb18e3cbd402473d5121bb69818b1ca90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f0f2ebfb61cea66fcb0386e02f08fa088d9aac")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioMedDocManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioMedDocManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
